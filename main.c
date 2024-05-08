52aa04ede83eed23daca4e5f61228ee8b2b11460
Hiroyuki OYAMA <oyama@module.jp>
2024-05-09 00:05:40 +0900

    Register access changed to via hardware_structs

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <hardware/regs/addressmap.h>
+#include <hardware/regs/usb.h>
+#include <hardware/structs/usb.h>
 #include <pico/stdlib.h>
 #include <stdio.h>
 #include <tusb.h>
@@ -6,9 +7,6 @@
 #if defined(RASPBERRYPI_PICO_W)
 #include <pico/cyw43_arch.h>
 #endif
-
-#define USB_SIE_STATUS_REG        ((volatile uint32_t *)(USBCTRL_REGS_BASE + 0x50))  // SIE status register
-#define USB_SIE_STATUS_SUSPENDED  0b00000000000000000000000000010000                 // Bus in suspended state
 
 #define COLOR_RED(format)    ("\e[31m" format "\e[0m")
 #define COLOR_GREEN(format)  ("\e[32m" format "\e[0m")
@@ -24,7 +22,7 @@ static void init_vbus_gpio(void) {
 }
 
 static bool is_usb_sie_status_suspended(void) {
-    return *USB_SIE_STATUS_REG & USB_SIE_STATUS_SUSPENDED;
+    return usb_hw->sie_status & USB_SIE_STATUS_SUSPENDED_BITS;
 }
 
 static bool vbus_supplied(void) {
@@ -45,7 +43,7 @@ int main(void) {
         printf("TinyUSB %s, ",
             tud_ready() ? COLOR_GREEN("ready") : COLOR_RED("not ready"));
         printf("USB_SIE_STATUS=0x%08lX %s, ",
-            *USB_SIE_STATUS_REG,
+            usb_hw->sie_status,
             (!is_usb_sie_status_suspended() ? COLOR_GREEN("connect") : COLOR_RED("disconnect")));
         printf("VBUS %s\n",
             vbus_supplied() ? COLOR_GREEN("high") : COLOR_RED("low"));
